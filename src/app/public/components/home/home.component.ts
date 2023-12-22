bd3248b2aec666167f2a3e2891c475c3fdf4c68c
Leonardo <leo.sopranzetti@hotmail.com>
2023-12-22 10:44:53 -0300

    paginação

--- src/app/public/components/home/home.component.ts
+++ src/app/public/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 import { PublicService } from '../../services/public.service';
 import { catchError, tap } from 'rxjs';
@@ -18,8 +18,15 @@ export class HomeComponent implements OnInit {
   numberOfResults: number = 0;
   openOrClosed: boolean = false;
   allGyms: Gym[] = [];
+  actualGymPage: Gym[] = [];
+  itemsPerPage: number = 9; 
+  currentPage: number = 1; 
+  totalPages: number = 0; 
+  totalPagesToShow: number = 3;
 
-  constructor(private fb: FormBuilder, private publicService: PublicService) { 
+  activePage: number | null = null;
+
+  constructor(private fb: FormBuilder, private publicService: PublicService, private renderer: Renderer2) { 
     this.myForm = this.fb.group({
       selectedOption: [''],
       openOrClosed: [false]
@@ -29,6 +36,7 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
     this.manipulationOfForm();
     this.getAllGyms();
+    this.setPage(1);
 
   }
 
@@ -44,16 +52,39 @@ export class HomeComponent implements OnInit {
   }
 
   public getAllGyms(){
+    this.actualGymPage = [];
+    this.allGyms = [];
 
     this.publicService.getGyms().pipe(
       tap((res : any ) => {
-        this.allGyms = res.locations.slice(0, 160);
+        this.allGyms = res.locations;
+        this.totalPages = Math.ceil(this.allGyms.length / this.itemsPerPage);
+        this.actualGymPage = this.getPaginatedItems();
       }),
       catchError((error)=> {
         return error
       })
     ).subscribe()
 
+  }
+
+  getPaginatedItems(): any[] {
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    const endIndex = startIndex + this.itemsPerPage;
+    return this.allGyms.slice(startIndex, endIndex);
+  }
+
+  setPage(page: number) {
+    if (page >= 1 && page <= this.totalPages) {
+      this.currentPage = page;
+      this.actualGymPage = this.getPaginatedItems();
+    }
+
+    this.activePage = this.currentPage;
+  }
+
+  totalPagesArray(): number[] {
+    return new Array(this.totalPages).fill(0).map((_, index) => index + 1);
   }
 
 }
